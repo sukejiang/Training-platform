fe405fb4000c8c923252591dc7da81841c67de7e
jiang <joyqifeng153@qq.com>
2023-03-18 00:01:30 +0800

    "selenium 基本方法"

--- selenium_01.py
+++ selenium_01.py
@@ -1,13 +1,26 @@
 from selenium import webdriver
-from time import *
 from selenium.webdriver.common.by import By
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-we = webdriver.Chrome(options=options)
-we = webdriver.Chrome()
-we.get('http://dev.znk.group/training/')
-we.find_element(By.ID,"txtuserid").clear()
-we.find_element(By.ID,"txtuserid").send_keys("123")
-sleep(20)
-we.close()
+driver = webdriver.Chrome() #获取浏览器控制
+
+driver.get('https://baidu.com') #获取网页的地址
+
+driver.maximize_window() #最大化浏览器
+
+driver.refresh()  #刷新浏览器
+
+# driver.back()  #返回上一页
+#
+# driver.forward() #前进
+
+title = driver.title
+print("标题：",title)
+
+driver.get_screenshot_as_file('百度首页.png') #截图网页
+
+html = driver.page_source
+
+print("源码：",html)  #获取源码
+
+driver.find_element(By.ID,"")
+driver.quit()
